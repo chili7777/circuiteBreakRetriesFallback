c37ed9be249ef9b57f35a876825e5b4df1d560a0
chili7777 <94617171+chili7777@users.noreply.github.com>
2026-09-11 08:36:10 -0500

    feat: add orders application

---
+++ orders-service/src/main/java/dev/chili7777/lab/orders/OrdersApplication.java
@@ -0,0 +1,5 @@
+package dev.chili7777.lab.orders;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class OrdersApplication { public static void main(String[] args) { SpringApplication.run(OrdersApplication.class, args); } }
